eb6ebaf2bf9bceea3132c6cd71225c9650fe3cbd
Tin Nguyen <tinnguyen263@outlook.com>
2018-06-05 23:41:24 +0700

    App now use firebase npm instead of js cdn

--- src/api/firebase-app.js
+++ src/api/firebase-app.js
@@ -1,1 +1,14 @@
+import firebase from '../../node_modules/@firebase/app';
+import '../../node_modules/@firebase/database';
+
+// noinspection SpellCheckingInspection
+export const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyCcnboD6v1Qdze8In091sos7N3bkZ3MPko',
+  authDomain: 'project-my-kanban.firebaseapp.com',
+  databaseURL: 'https://project-my-kanban.firebaseio.com',
+  projectId: 'project-my-kanban',
+  storageBucket: 'project-my-kanban.appspot.com',
+  messagingSenderId: '964738474969',
+});
+
 export const database = firebaseApp.database();
