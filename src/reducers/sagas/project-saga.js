f02364ebbb41e9f402cf2e4311d64e66680a80c7
Tin Nguyen <tinnguyen263#@outlook.com>
2018-06-12 23:34:28 +0700

    add some common sagas

--- src/reducers/sagas/project-saga.js
+++ src/reducers/sagas/project-saga.js
@@ -1,29 +1,31 @@
-import {call, put} from 'redux-saga/effects';
+import {call, put, select, all} from 'redux-saga/effects';
 import {API} from '../../api/index';
 import {Actions} from '../../actions/index';
 
-export function* createProjectAndCloseDiaog(action) {
+function* computeNewProjectId(baseId) {
+  let nTry = 0;
+  let nextId = baseId;
+  let hasId = yield call(API.project.hasId, nextId);
+  while (hasId) {
+    nextId = `${baseId}-${nTry}`;
+    nTry++;
+    hasId = yield call(API.project.hasId, nextId);
+  }
+  return nextId;
+}
+
+export function* createProjectAndCloseDialog(action) {
   let project = action.payload.project;
   try {
-    // check new id
-    let nTry = 0;
-    let nextId = project.id;
-    let hasId = yield call(API.project.hasId, nextId);
-    while (hasId) {
-      nextId = `${project.id}-${nTry}`;
-      nTry++;
-      hasId = yield call(API.project.hasId, nextId);
-    }
-    project.id = nextId;
-    const currentUser = yield call(API.auth.getCurrentUser);
+    project.id = yield call(computeNewProjectId, project.id);
+    const currentUser = yield select((state) => state.auth.user);
     project.owner = currentUser.uid;
 
-    // save to firebase
-    yield call(API.project.saveProject, project);
-
     // save to store
-    const projectList = yield call(API.project.getProjectsOfCurrentUser);
-    yield put(Actions.project.saveProjectsToState(projectList));
+    yield put(Actions.project.saveProjectToState(project));
+
+    // push to cloud
+    yield put(Actions.project.pushOne(project.id));
 
     // close dialog
     yield put(Actions.app.hideDialog());
@@ -36,6 +38,67 @@ export function* createProjectAndCloseDiaog(action) {
 export function* cancelCreateProjectDialog() {
   try {
     yield put(Actions.app.hideDialog());
+  } catch (e) {
+    yield put(Actions.app.showToast(e.message));
+  }
+}
+
+
+// export function* createProject(project) {
+//   try {
+//     project.id = yield call(computeNewProjectId, project.id);
+//     const currentUser = yield call(API.auth.getCurrentUser);
+//     project.owner = currentUser.uid;
+//     yield call(saveProject, project);
+//   } catch (e) {
+//     yield put(Actions.app.showToast(e.message));
+//   }
+// }
+//
+// export function* saveProject(project) {
+//   try {
+//     // save to firebase
+//     project = yield call(API.project.saveOrUpdate, project);
+//     // save to store
+//     yield put(Actions.project.saveProjectToState(project));
+//   } catch (e) {
+//     yield put(Actions.app.showToast(e.message));
+//   }
+// }
+
+export function* pushAll() {
+  try {
+    let projects = yield select((state) => state.userData.projects);
+    yield all(Object.values(projects).map((project) => put(Actions.project.pushOne(project.id))));
+  } catch (e) {
+    yield put(Actions.app.showToast(e.message));
+  }
+}
+
+export function* pullAll() {
+  try {
+    let projects = yield call(API.project.getProjectsOfCurrentUser);
+    yield all(Object.values(projects).map((project) => put(Actions.project.saveProjectToState(project))));
+  } catch (e) {
+    yield put(Actions.app.showToast(e.message));
+  }
+}
+
+export function* pushOne(action) {
+  try {
+    let projectId = action.payload.projectId;
+    let project = yield select((state) => state.userData.projects[projectId]);
+    yield call(API.project.saveProject, project);
+  } catch (e) {
+    yield put(Actions.app.showToast(e.message));
+  }
+}
+
+export function* pullOne(action) {
+  try {
+    let projectId = action.payload.projectId;
+    let project = yield call(API.project.getSpecificProjectOfCurrentUser, projectId);
+    yield put(Actions.project.saveProjectToState(project));
   } catch (e) {
     yield put(Actions.app.showToast(e.message));
   }
