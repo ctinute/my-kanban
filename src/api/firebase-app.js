d3886b828a5ea3db6c3ae7069df759ca1ab8ed8f
Tin Nguyen <tinnguyen263#@outlook.com>
2018-06-17 08:19:18 +0700

    Fix firebase authentication

--- src/api/firebase-app.js
+++ src/api/firebase-app.js
@@ -1,12 +1,2 @@
-// noinspection SpellCheckingInspection
-export const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyCcnboD6v1Qdze8In091sos7N3bkZ3MPko',
-  authDomain: 'project-my-kanban.firebaseapp.com',
-  databaseURL: 'https://project-my-kanban.firebaseio.com',
-  projectId: 'project-my-kanban',
-  storageBucket: 'project-my-kanban.appspot.com',
-  messagingSenderId: '964738474969',
-});
-
 export const database = firebase.database();
 export const authentication = firebase.auth();
