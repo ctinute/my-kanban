270758926b37ef8b7d28dfae27868391e380d378
Tin Nguyen <tinnguyen263@outlook.com>
2018-06-21 02:44:03 +0700

    [UI] Project Dashboard

--- src/screens/mk-project.js
+++ src/screens/mk-project.js
@@ -1,7 +1,4 @@
 import {html} from '@polymer/lit-element';
-import '@polymer/app-layout/app-header-layout/app-header-layout';
-import '@polymer/app-layout/app-header/app-header';
-import '@polymer/app-layout/app-toolbar/app-toolbar';
 import '@polymer/paper-icon-button';
 import '@polymer/paper-card';
 import {Actions} from '../actions';
@@ -49,9 +46,60 @@ export default class MkProject extends MkScreen {
   _renderStyles() {
     return html`
       <style>
+        :host {
+          width: 100%;
+          height: 100%;
+          display: block;
+          overflow-y: scroll;
+        }
         .section {
-            width: 100%;
-            margin: 16px 0;
+          width: 100%;
+          padding: 0 16px;
+          box-sizing: border-box;
+        }
+        .list.phase {}
+        .item.phase {
+          display: flex;
+          align-items: center;
+          flex-direction: row;
+          width: 100%;
+          margin: 8px 0;
+          padding: 0 16px;
+          overflow: hidden;
+          transition: margin 0.3s 0.2s, box-shadow 0.5s ease;
+          will-change: margin, box-shadow;
+        }
+        .item.phase > .title, 
+        .item.phase > .actions {
+          display: inline-block;
+        }
+        .item.phase > .title {
+          font-size: 16px;
+          height: 48px;
+          line-height: 48px;
+          text-decoration: none;
+          flex-grow: 1;
+        }
+        .item.phase > .actions {
+          color: black;
+          transform: translateX(150%);
+          transition: transform 0.3s 0.2s; 
+        }
+        .item.phase > .actions > iron-icon {
+          margin: 0 4px;
+          padding: 4px;
+          border-radius: 50%;
+        }
+        .item.phase > .actions > iron-icon:hover {
+          background-color: #B0BEC5;
+          cursor: pointer;
+        }
+        .item.phase:hover {
+          /*margin: 16px 0;*/
+          box-shadow: var(--shadow-elevation-4dp_-_box-shadow);
+        }
+        .item.phase:hover > .actions {
+          transform: translateX(0); 
         }
       </style>
     `;
@@ -62,48 +110,47 @@ export default class MkProject extends MkScreen {
   }
 
   _renderOverview(project) {
-    let activePhaseId = project.currentPhase;
-    let activePhase = project.phases[activePhaseId];
     return html`
-      <div class="card-header">
-        Current phase: ${activePhase.name}
-      </div>
-      <div class="card-content">
-        some overall info
-      </div>
-      <div class="card-actions">
-        <div class="horizontal justified">
-          <paper-button on-click="${() => this._navigateProjectDetailPage(project.name, project.id, activePhaseId)}">View detail</paper-button>
-        </div>
-      </div>
-    `;
+      <h2 class="sectiom-header">Overview</h2>
+      <div class="section-content">
+        <span><i>Nothing yet</i></span>
+      </div>`;
   }
 
-  _renderPhaseList(projectId, phases) {
-    if (phases) {
-      return html`
-        <ul>
-          ${Object.values(phases).map((phase) => html`<li><a href="/u/${projectId}/${phase.id}">${phase.name}</a></li>`)}
-        </ul>`;
-    } else {
-      return html`You don't have any phase yet.<br/> Press button bellow to create new one.`;
-    }
+  _renderPhaseItem(phase, projectId) {
+    return html`
+      <paper-card class="item phase" elevation="0">
+        <a class="title" href="/u/${projectId}/${phase.id}">${phase.name}</a>
+        <div class="actions">
+          <iron-icon icon="icons:create"></iron-icon>
+          <iron-icon icon="icons:delete"></iron-icon>
+        </div>
+      </paper-card>
+    `;
   }
 
   _renderPhases(project) {
+    let phaseList = project.phases ?
+      html`
+        <div class="list phases">
+          ${Object.values(project.phases).map((phase) => this._renderPhaseItem(phase, project.id))}
+        </div>` :
+      html`You don't have any phase yet.<br/> Press button bellow to create new one.`;
     return html`
-      <div class="card-header">
-        Phases
-      </div>
-      <div class="card-content">
-        ${this._renderPhaseList(project.id, project.phases)}
-      </div>
-      <div class="card-actions">
-        <div class="horizontal justified">
-            <paper-button ripple on-click="${() => this._openCreatePhaseDialog(project.id)}">New phase...</paper-button>
+      <h2 class="sectiom-header">Phases</h2>
+      <div class="section-content">
+        <div class="phase current">
+          <h3>Current</h3>
+          ${this._renderPhaseItem(project.phases[project.currentPhase], project.id)}
         </div>
-      </div>
-    `;
+        <div id="phase others">
+          <h3>Others</h3>
+          ${phaseList}
+          <div class="list-actions">
+            <paper-button ripple on-click="${() => this._openCreatePhaseDialog(project.id)}">New phase...</paper-button>
+          </div>
+        </div>
+      </div>`;
   }
 
   _render({project}) {
@@ -112,13 +159,13 @@ export default class MkProject extends MkScreen {
     let phases = this._renderPhases(project);
     return html`
       ${styles}
-      <paper-card class="section overview">
+      <div class="section overview">
         ${overView}
-      </paper-card>
-      <paper-card class="section phases">
+      </div>
+      <div class="section phases">
         ${phases}
-      </paper-card>
-      <paper-card class="section"></paper-card>
+      </div>
+      <div class="section"></div>
     `;
   }
 }
