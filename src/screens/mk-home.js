f2532425aab94b9375b48ba69d98be6012225bf7
Tin Nguyen <tinnguyen263@outlook.com>
2018-06-24 13:36:49 +0700

    Home Screen now extends MkScreen class, fix app-toolbar overlap after login

--- src/screens/mk-home.js
+++ src/screens/mk-home.js
@@ -1,9 +1,7 @@
-import {PageViewElement} from './page-view-element.js';
 import {html} from '@polymer/lit-element';
-import {store} from '../store.js';
-import {connect} from 'pwa-helpers/connect-mixin.js';
+import {MkScreen} from './mk-screen';
 
-export default class MkHome extends connect(store)(PageViewElement) {
+export default class MkHome extends MkScreen {
   constructor() {
     super();
   }
@@ -17,19 +15,19 @@ export default class MkHome extends connect(store)(PageViewElement) {
 
   _render(props) {
     const style = html`
-            <style>
-                :host {
-                    width: 100%;
-                    height: 100%;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;                
-                }
-            </style>`;
+      <style>
+        :host {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;                
+        }
+      </style>`;
     return html`
-            ${style}
-            Home
-        `;
+      ${style}
+      Home
+    `;
   }
 }
 
